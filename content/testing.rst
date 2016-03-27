de80c6580effd30cf7292e1b0d1dadbe977445c4
Moshe Zadka <zadka.moshe@gmail.com>
2016-03-27 13:54:43 -0700

    Temp commit

---
+++ content/testing.rst
@@ -0,0 +1,14 @@
+Testing tools
+=============
+
+:date: 2016-3-27 13:45
+
+* Built-in unittest_ library
+* pytest_ test runner
+* Trial_, the Twisted test-runner
+* Mocking_ library
+
+.. _unittest: https://docs.python.org/2/library/unittest.html
+.. _pytest: https://pytest.org/latest/
+.. _Trial: https://twistedmatrix.com/documents/current/core/howto/trial.html
+.. _Mocking: https://pypi.python.org/pypi/mock
---
+++ pelicanconf.py
@@ -0,0 +1,28 @@
+from __future__ import unicode_literals
+
+AUTHOR = u'Moshe Zadka'
+SITENAME = u'Weekly Python Tip'
+SITEURL = 'https://weeklypythontip.github.io'
+
+PATH = 'content'
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (("Moshe'z", 'https://moshez.wordpress.com/'),
+         )
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 10
+RELATIVE_URLS = True
